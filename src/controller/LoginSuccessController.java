17ae5943d032ae857c53e7bfcd0a37a99c64e705
chucoding <chucoding@gmail.com>
2020-08-18 21:52:52 +0900

    LoginController, LoginSuccessController 구현

--- src/controller/LoginSuccessController.java
+++ src/controller/LoginSuccessController.java
@@ -8,15 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class LoginController extends HttpServlet {
+@WebServlet("/loginSuccess")
+public class LoginSuccessController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/contents/MAN/html/naverlogin.html").forward(req, resp);
+		req.getRequestDispatcher("/contents/MAN/html/callback.html").forward(req, resp);
 	}
-
 }
 
 
