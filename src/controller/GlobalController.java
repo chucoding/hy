514e0710de7665553a4bd4edee62fe67d91bfdf6
chucoding <chucoding@gmail.com>
2020-08-31 01:41:25 +0900

    다국어 처리 테스트 페이지 추가

--- src/controller/GlobalController.java
+++ src/controller/GlobalController.java
@@ -8,14 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class LoginController extends HttpServlet {
-
+/**
+ * Servlet implementation class GlobalController
+ */
+@WebServlet("/globalTest")
+public class GlobalController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/contents/MAN/html/naverlogin.html").forward(req, resp);
+		req.getRequestDispatcher("/WEB-INF/views/globalTest.jsp").forward(req, resp);
 	}
 }
-
-
